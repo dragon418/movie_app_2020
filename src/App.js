f692abe9f28ec4229df7f355623d500aa141da16
dragon418 <obong.an@lge.com>
2020-12-17 10:28:52 +0900

    remove unsed lines

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: 'Kimchi',
+    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png'
+  },
+  {
+    id:2,
+    name: "Bibimbap",
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Korean_cuisine-Jeonju_bibimbap-01.jpg/440px-Korean_cuisine-Jeonju_bibimbap-01.jpg'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        { foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} picture={dish.image}/>
+        ))}
+      </div>
   );
 }
 
